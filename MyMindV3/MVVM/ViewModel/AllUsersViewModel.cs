34e2ef20c575790e8b813b3a361e43a8810f3d4c
Paul Johnson <paul@snuberry-software.co.uk>
2017-01-19 14:29:41 +0000

    Fix for mvvmlight

---
+++ MyMindV3/MVVM/ViewModel/AllUsersViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace MvvmFramework.ViewModel
+{
+    public class AllUsersViewModel : BaseViewModel
+    {
+        private int _allUsersTotal;
+
+        public int AllUsersTotal
+        {
+            get { return _allUsersTotal; }
+            set
+            {
+                if (value != _allUsersTotal)
+                {
+                    Set(() => AllUsersTotal, ref _allUsersTotal, value, true); 
+                }
+            }
+        }
+    }
+}
