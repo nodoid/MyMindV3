195053677f2d601026b56db9e90c51b083d0ccef
Paul Johnson <paul@snuberry-software.co.uk>
2017-01-19 15:32:36 +0000

    Fill the blanks

--- MyMindV3/MVVM/ViewModelLocator.cs
+++ MyMindV3/MVVM/ViewModelLocator.cs
@@ -15,6 +15,7 @@
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Ioc;
 using Microsoft.Practices.ServiceLocation;
+using MvvmFramework.ViewModel;
 
 namespace MvvmFramework
 {
@@ -27,32 +28,142 @@ namespace MvvmFramework
         /// <summary>
         /// Initializes a new instance of the ViewModelLocator class.
         /// </summary>
+        /// 
+        public const string AllRegisteredUsersKey = "AllRegisteredUsers";
+        public const string LoginPinCodeKey = "LoginPinCode";
+        public const string LoginKey = "Login";
+        public const string MyChatKey = "MyChat";
+        public const string MyClinicianKey = "MyClinician";
+        public const string MyFileKey = "MyFile";
+        public const string MyJourneyKey = "MyJourney";
+        public const string MyLimitedMindKey = "MyLimitedMind";
+        public const string MyMindKey = "MyMind";
+        public const string MyPatientKey = "MyPatient";
+        public const string MyPlansKey = "MyPlans";
+        public const string MyProfileKey = "MyProfile";
+        public const string MyResourcesKey = "MyResources";
+        public const string SignupKey = "SignUp";
+
         public ViewModelLocator()
         {
             ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
 
-            ////if (ViewModelBase.IsInDesignModeStatic)
-            ////{
-            ////    // Create design time view services and models
-            ////    SimpleIoc.Default.Register<IDataService, DesignDataService>();
-            ////}
-            ////else
-            ////{
-            ////    // Create run time view services and models
-            ////    SimpleIoc.Default.Register<IDataService, DataService>();
-            ////}
-
-            SimpleIoc.Default.Register<MainViewModel>();
+            SimpleIoc.Default.Register<AllRegisteredUserViewModel>();
+            SimpleIoc.Default.Register<LoginPinCodeViewModel>();
+            SimpleIoc.Default.Register<LoginViewModel>();
+            SimpleIoc.Default.Register<MyChatViewModel>();
+            SimpleIoc.Default.Register<MyClinicianViewModel>();
+            SimpleIoc.Default.Register<MyFileViewModel>();
+            SimpleIoc.Default.Register<MyJourneyViewModel>();
+            SimpleIoc.Default.Register<MyLimitedMindViewModel>();
+            SimpleIoc.Default.Register<MyMindViewModel>();
+            SimpleIoc.Default.Register<MyPatientViewModel>();
+            SimpleIoc.Default.Register<MyPlansViewModel>();
+            SimpleIoc.Default.Register<MyProfileViewModel>();
+            SimpleIoc.Default.Register<MyResourcesViewModel>();
+            SimpleIoc.Default.Register<SignUpViewModel>();
         }
 
-        public MainViewModel Main
+        public AllRegisteredUserViewModel AllRegisteredUsers
         {
             get
             {
-                return ServiceLocator.Current.GetInstance<MainViewModel>();
+                return ServiceLocator.Current.GetInstance<AllRegisteredUserViewModel>();
             }
         }
-        
+
+        public LoginPinCodeViewModel LoginPinCode
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<LoginPinCodeViewModel>();
+            }
+        }
+        public LoginViewModel Login
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<LoginViewModel>();
+            }
+        }
+        public MyChatViewModel MyChat
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MyChatViewModel>();
+            }
+        }
+        public MyClinicianViewModel MyClinician
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MyClinicianViewModel>();
+            }
+        }
+        public MyFileViewModel MyFile
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MyFileViewModel>();
+            }
+        }
+        public MyJourneyViewModel MyJourney
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MyJourneyViewModel>();
+            }
+        }
+        public MyLimitedMindViewModel MyLimitedMind
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MyLimitedMindViewModel>();
+            }
+        }
+        public MyMindViewModel MyMind
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MyMindViewModel>();
+            }
+        }
+        public MyPatientViewModel MyPatient
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MyPatientViewModel>();
+            }
+        }
+        public MyPlansViewModel MyPlans
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MyPlansViewModel>();
+            }
+        }
+        public MyProfileViewModel MyProfile
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MyProfileViewModel>();
+            }
+        }
+        public MyResourcesViewModel MyResources
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MyResourcesViewModel>();
+            }
+        }
+        public SignUpViewModel SignUp
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<SignUpViewModel>();
+            }
+        }
+
         public static void Cleanup()
         {
             // TODO Clear the ViewModels
