6f2c639031fdf63891fd23e5fb737d08e9e57a70
Paul Johnson <paul@snuberry-software.co.uk>
2017-01-19 14:28:55 +0000

    imported and hacked

---
+++ MyMindV3/MVVM/Database/SystemUserDB.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using SQLite.Net;
+using System.Diagnostics;
+
+namespace MvvmFramework
+{
+    public class SystemUserDB
+    {
+        private SQLiteConnection _sqlConnection;
+
+        public SystemUserDB()
+        {
+            _sqlConnection = DependencyService.Get<ISQLite>().GetConnection();
+            _sqlConnection.CreateTable<SystemUser>();
+        }
+
+        // get all system users
+        public IEnumerable<SystemUser> GetSystemUsers()
+        {
+            return (from t in _sqlConnection.Table<SystemUser>()
+                    select t).ToList();
+        }
+
+        // get specific system user
+        public SystemUser GetSystemUser(int id)
+        {
+            return _sqlConnection.Table<SystemUser>().FirstOrDefault(t => t.UserID == id);
+        }
+
+        // delete system user - this is for dev purposes
+        public void DeleteSystemUser(int id)
+        {
+            _sqlConnection.Delete<SystemUser>(id);
+        }
+
+        // update system user
+        
+    }
+}
