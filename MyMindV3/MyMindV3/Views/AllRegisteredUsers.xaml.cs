f3a21dc6278e22c658aa9b98ba27d768f49a63bd
Paul Johnson <paul@snuberry-software.co.uk>
2017-01-19 16:52:48 +0000

    updated

--- MyMindV3/MyMindV3/Views/AllRegisteredUsers.xaml.cs
+++ MyMindV3/MyMindV3/Views/AllRegisteredUsers.xaml.cs
@@ -20,15 +20,6 @@ namespace MyMindV3.Views
             BindingContext = ViewModel;
 
             _allUsersVM.AllUsersTotal = _users.Count();
-
-            /*
-            AllRegisteredUsersListView.ItemSelected += async (sender, e) =>
-            {
-                if (e.SelectedItem == null) return; // don't do anything if we just de-selected the row
-
-                await DisplayAlert("Selected a chat client", "You selected " + "test", "OK");
-                ((ListView)sender).SelectedItem = null; // de-select the row
-            };*/
         }
     }
 }
