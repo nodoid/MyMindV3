2d50e377c1a90999827d589fd5841f64ec4860f8
Paul Johnson <paul@snuberry-software.co.uk>
2017-01-20 13:28:50 +0000

    Add relay commands

--- MyMindV3/MVVM/ViewModel/MyLimitedMindViewModel.cs
+++ MyMindV3/MVVM/ViewModel/MyLimitedMindViewModel.cs
@@ -1,15 +1,32 @@
-﻿using GalaSoft.MvvmLight.Views;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Views;
 
 namespace MvvmFramework.ViewModel
 {
     public class MyLimitedMindViewModel : BaseViewModel
     {
+        INavigationService navService;
         public MyLimitedMindViewModel(INavigationService nav)
-        { }
+        {
+            navService = nav;
+        }
+
+        RelayCommand showMyProfileCommand;
+        public RelayCommand ShowMyProfileCommand
+        {
+            get
+            {
+                return showMyProfileCommand ?? (new RelayCommand(() => navService.NavigateTo(ViewModelLocator.MyProfileKey)));
+            }
+        }
+
+        RelayCommand showMyResourcesCommand;
+        public RelayCommand ShowMyResourcesCommand
+        {
+            get
+            {
+                return showMyResourcesCommand ?? (new RelayCommand(() => navService.NavigateTo(ViewModelLocator.MyResourcesKey)));
+            }
+        }
     }
 }
