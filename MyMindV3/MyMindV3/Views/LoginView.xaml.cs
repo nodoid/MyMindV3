601086f5359551812abe0be82da02f1855278d1c
Paul Johnson <paul@snuberry-software.co.uk>
2017-01-20 13:30:56 +0000

    Remove emailRegex

--- MyMindV3/MyMindV3/Views/LoginView.xaml.cs
+++ MyMindV3/MyMindV3/Views/LoginView.xaml.cs
@@ -9,9 +9,6 @@ namespace MyMindV3.Views
     {
         LoginViewModel ViewModel => App.Locator.Login;
         private int _imgCount = 1;
-        const string emailRegex = @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
-        @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$";
-
 
         // construct
         public LoginView()
