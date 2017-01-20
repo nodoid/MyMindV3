1e6564fefad6ca1f309094455e58bc74aa7d58a5
Paul Johnson <paul@snuberry-software.co.uk>
2017-01-20 16:14:49 +0000

    Brought over from the forms pcl

---
+++ MyMindV3/MVVM/Models/Encryption.cs
@@ -0,0 +1,15 @@
+﻿namespace MvvmFramework.Models
+{
+    public class Encryption
+    {
+        public string Category { get; set; }
+
+        public string Value { get; set; }
+
+        public string Key { get; set; }
+
+        public string IV { get; set; }
+
+        public string Hashmac { get; set; }
+    }
+}
