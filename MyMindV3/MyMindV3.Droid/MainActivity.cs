d3a77cad7d7861a1b45d1d6a69238cb660b19e1a
Paul Johnson <paul@snuberry-software.co.uk>
2017-01-30 01:24:16 +0000

    Add code from MyMind2

--- MyMindV3/MyMindV3.Droid/MainActivity.cs
+++ MyMindV3/MyMindV3.Droid/MainActivity.cs
@@ -1,25 +1,31 @@
-﻿using System;
-
+﻿
 using Android.App;
+using Android.Content;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using ImageCircle.Forms.Plugin.Droid;
+using Xamarin.Forms;
 
 namespace MyMindV3.Droid
 {
-    [Activity(Label = "MyMindV3", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    [Activity(ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.KeyboardHidden, WindowSoftInputMode = Android.Views.SoftInput.AdjustResize)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
     {
+        public static MainActivity Activity { get; private set; }
+
+        public static ISharedPreferences Prefs { get; set; }
+
         protected override void OnCreate(Bundle bundle)
         {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
-
             base.OnCreate(bundle);
 
+            MainActivity.Activity = this;
+            Prefs = GetSharedPreferences("MyMindV3", FileCreationMode.Private);
             global::Xamarin.Forms.Forms.Init(this, bundle);
+            App.ScreenSize = new Size(Resources.DisplayMetrics.WidthPixels / Resources.DisplayMetrics.Density,
+                Resources.DisplayMetrics.HeightPixels / Resources.DisplayMetrics.Density);
+            ImageCircleRenderer.Init();
+            PullToRefreshViewRenderer.Init();
             LoadApplication(new App());
         }
     }
