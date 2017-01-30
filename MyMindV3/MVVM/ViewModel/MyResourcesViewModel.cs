755cec858700113cc0eecb5ee328df11ef684d7d
Paul Johnson <paul@snuberry-software.co.uk>
2017-01-30 01:22:40 +0000

    Modify resources view model for the new UI

--- MyMindV3/MVVM/ViewModel/MyResourcesViewModel.cs
+++ MyMindV3/MVVM/ViewModel/MyResourcesViewModel.cs
@@ -1,13 +1,8 @@
 ﻿using GalaSoft.MvvmLight.Views;
 using MvvmFramework.Models;
 using MvvmFramework.Webservices;
-using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
 
 namespace MvvmFramework.ViewModel
 {
@@ -19,11 +14,54 @@ namespace MvvmFramework.ViewModel
             navService = nav;
         }
 
+        string searchPostcode;
+        public string SearchPostcode
+        {
+            get { return searchPostcode; }
+            set { Set(() => SearchPostcode, ref searchPostcode, value, true); }
+        }
+
+        double longitude;
+        public double Longitude
+        {
+            get { return longitude; }
+            set { Set(() => Longitude, ref longitude, value, true); }
+        }
+
+        double latitude;
+        public double Latitude
+        {
+            get { return latitude; }
+            set { Set(() => Latitude, ref latitude, value, true); }
+        }
+
+        public List<Postcodes> GetAvailablePostcodes
+        {
+            get { return GetData.GetSurroundingPostcodes(SearchPostcode).Result; }
+        }
+
+        public string GetMyPostcode
+        {
+            get
+            {
+                var postcode = GetData.GetPostcode(Longitude, Latitude).Result;
+                SearchPostcode = postcode;
+                return postcode;
+            }
+        }
+
         IEnumerable<ResourceModel> resources;
         public IEnumerable<ResourceModel> Resources
         {
-            get { Resources = new UsersWebservice().GetResources().Result ; return resources; }
+            get { Resources = new UsersWebservice().GetResources().Result; return resources; }
             set { Set(() => Resources, ref resources, value); }
+        }
+
+        public string SearchBy { get; set; }
+
+        public IEnumerable<ResourceModel> GetSearchByResources
+        {
+            get { return Resources.Where(t => t.ResourceCategory == SearchBy); }
         }
 
         public List<string> FillCategories
