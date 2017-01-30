d9d0356407f60090c4438812fc449043f2c891c8
Paul Johnson <paul@snuberry-software.co.uk>
2017-01-30 01:26:30 +0000

    Add code from  MyMind2 and the new modal system

--- MyMindV3/MyMindV3.iOS/AppDelegate.cs
+++ MyMindV3/MyMindV3.iOS/AppDelegate.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 
 using Foundation;
+using ImageCircle.Forms.Plugin.iOS;
 using UIKit;
+using Xamarin.Forms;
 
 namespace MyMindV3.iOS
 {
@@ -13,16 +15,29 @@ namespace MyMindV3.iOS
     [Register("AppDelegate")]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
-        //
-        // This method is invoked when the application has loaded and is ready to run. In this 
-        // method you should instantiate the window, load the UI into it and then make the window
-        // visible.
-        //
-        // You have 17 seconds to return from this method, or iOS will terminate your application.
-        //
+        public bool Retina { get; private set; }
+
+        public nfloat ScreenX { get; private set; }
+
+        public nfloat ScreenY { get; private set; }
+
+        public bool IsIPhone { get; private set; }
+
+        public static AppDelegate Self { get; private set; }
+
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
+            AppDelegate.Self = this;
             global::Xamarin.Forms.Forms.Init();
+            ImageCircleRenderer.Init();
+            PullToRefreshViewRenderer.Init();
+            App.ScreenSize = new Size(UIScreen.MainScreen.Bounds.Width, UIScreen.MainScreen.Bounds.Height);
+            IsIPhone = UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Phone;
+
+            Retina = UIScreen.MainScreen.RespondsToSelector(new ObjCRuntime.Selector("scale")) ? true : false;
+
+            ScreenX = UIScreen.MainScreen.Bounds.Width;
+            ScreenY = UIScreen.MainScreen.Bounds.Height
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
