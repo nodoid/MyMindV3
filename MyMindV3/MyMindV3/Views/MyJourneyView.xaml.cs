3bf3fa55c498d600e70a5248ede4a2fdae377160
Paul Johnson <paul@snuberry-software.co.uk>
2017-01-30 01:31:14 +0000

    check if appointments returned are null

--- MyMindV3/MyMindV3/Views/MyJourneyView.xaml.cs
+++ MyMindV3/MyMindV3/Views/MyJourneyView.xaml.cs
@@ -19,6 +19,7 @@ namespace MyMindV3.Views
         {
             InitializeComponent();
             BindingContext = ViewModel;
+            ViewModel.IsConnected = App.Self.IsConnected;
             LoadData();
             id = ViewModel.SystemUser.IsAuthenticated == 3 ? ViewModel.ClinicianUser.HCPID : ViewModel.SystemUser.ICANN;
             btnWebview.IsVisible = !string.IsNullOrEmpty(id);
@@ -49,56 +50,58 @@ namespace MyMindV3.Views
             ViewModel.HCP = ViewModel.ClinicianUser.HCPID;
             appointments = ViewModel.Appointments;
 
-            var sortedList = appointments.OrderByDescending(o => o.AppointmentDateTime.CleanDate()).ToList();
-
-            var dayOfYear = DateTime.Now.DayOfYear;
-            var lower = dayOfYear - (int)DayOfWeek.Monday;
-            var upper = dayOfYear + (int)DayOfWeek.Friday;
-            var thisWeek = 0;
-
-            var thisMonth = sortedList?.Count(t => t.AppointmentDateTime.CleanDate().Month == DateTime.Now.Month);
-
-            if (thisMonth != 0)
+            if (appointments != null)
             {
-                // we have an appointment this month
-                var thisMonthsApps = sortedList.Where(t => t.AppointmentDateTime.CleanDate().Month == DateTime.Now.Month).ToList();
-                foreach (var app in thisMonthsApps)
+                var sortedList = appointments.OrderByDescending(o => o.AppointmentDateTime.CleanDate()).ToList();
+
+                var dayOfYear = DateTime.Now.DayOfYear;
+                var lower = dayOfYear - (int)DayOfWeek.Monday;
+                var upper = dayOfYear + (int)DayOfWeek.Friday;
+                var thisWeek = 0;
+
+                var thisMonth = sortedList?.Count(t => t.AppointmentDateTime.CleanDate().Month == DateTime.Now.Month);
+
+                if (thisMonth != 0)
                 {
-                    var date = app.AppointmentDateTime.CleanDate();
-                    var dayOfMonth = date.DayOfYear;
-                    if (dayOfMonth >= lower && dayOfMonth <= upper)
-                        thisWeek++;
+                    // we have an appointment this month
+                    var thisMonthsApps = sortedList.Where(t => t.AppointmentDateTime.CleanDate().Month == DateTime.Now.Month).ToList();
+                    foreach (var app in thisMonthsApps)
+                    {
+                        var date = app.AppointmentDateTime.CleanDate();
+                        var dayOfMonth = date.DayOfYear;
+                        if (dayOfMonth >= lower && dayOfMonth <= upper)
+                            thisWeek++;
+                    }
                 }
+
+                txtApptsWeek.Text = thisWeek.ToString();
+                txtApptsMonth.Text = thisMonth.ToString();
+
+                ClientAppointmentsListView.ItemsSource = sortedList;
+                ClientAppointmentsListView.IsPullToRefreshEnabled = true;
+                ClientAppointmentsListView.Refreshing += (object sender, EventArgs e) =>
+                 {
+                     ClientAppointmentsListView.IsRefreshing = true;
+                     ViewModel.ClientID = ViewModel.SystemUser.RIOID;
+                     ViewModel.HCP = ViewModel.ClinicianUser.HCPID;
+                     appointments = ViewModel.Appointments;
+                     Device.BeginInvokeOnMainThread(() =>
+                         {
+                             ClientAppointmentsListView.ItemsSource = null;
+                             ClientAppointmentsListView.ItemsSource = appointments;
+                         });
+                     ClientAppointmentsListView.IsRefreshing = false;
+                 };
+                ClientAppointmentsListView.ItemSelected += (sender, e) =>
+                {
+                    if (e.SelectedItem == null) return; // don't do anything if we just de-selected the row
+
+                    var appointment = e.SelectedItem as Appointment;
+
+                    //await DisplayAlert(Langs.MyJourney_AlertSelected, string.Format("{0} {1}", Langs.MyJourney_AlertSelectedMsg, appointment.AppointmentDateTime), Langs.Gen_OK);
+                    ((ListView)sender).SelectedItem = null; // de-select the row
+                };
             }
-
-            txtApptsWeek.Text = thisWeek.ToString();
-            txtApptsMonth.Text = thisMonth.ToString();
-
-            ClientAppointmentsListView.ItemsSource = sortedList;
-            ClientAppointmentsListView.IsPullToRefreshEnabled = true;
-            ClientAppointmentsListView.Refreshing += (object sender, EventArgs e) =>
-             {
-                 ClientAppointmentsListView.IsRefreshing = true;
-                 ViewModel.ClientID = ViewModel.SystemUser.RIOID;
-                 ViewModel.HCP = ViewModel.ClinicianUser.HCPID;
-                 appointments = ViewModel.Appointments;
-                 Device.BeginInvokeOnMainThread(() =>
-                     {
-                         ClientAppointmentsListView.ItemsSource = null;
-                         ClientAppointmentsListView.ItemsSource = t.Result;
-                     });
-                 appointments = t.Result;
-                 ClientAppointmentsListView.IsRefreshing = false;
-             };
-            ClientAppointmentsListView.ItemSelected += (sender, e) =>
-            {
-                if (e.SelectedItem == null) return; // don't do anything if we just de-selected the row
-
-                var appointment = e.SelectedItem as Classes.Appointment;
-
-                //await DisplayAlert(Langs.MyJourney_AlertSelected, string.Format("{0} {1}", Langs.MyJourney_AlertSelectedMsg, appointment.AppointmentDateTime), Langs.Gen_OK);
-                ((ListView)sender).SelectedItem = null; // de-select the row
-            };
         }
 
         void Handle_Clicked(object s, EventArgs e)
@@ -106,6 +109,6 @@ namespace MyMindV3.Views
             // do nothing
         }
 
-        
+
     }
 }
