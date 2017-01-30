34440fdcb040724c1b1c780ebe8a8bd939a534da
Paul Johnson <paul@snuberry-software.co.uk>
2017-01-30 01:26:12 +0000

    splash screen

---
+++ MyMindV3/MyMindV3.Droid/Splash.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using Android.App;
+using Android.OS;
+
+namespace MyMindV3.Droid
+{
+    [Activity(Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true, Icon = "@drawable/icon")]
+    public class Splash : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Thread.Sleep(500);
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
+
