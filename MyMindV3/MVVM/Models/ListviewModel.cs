890718daa24cf1863d01757660dfa46e2b558705
Paul Johnson <paul@snuberry-software.co.uk>
2017-01-30 01:21:15 +0000

    listview model

---
+++ MyMindV3/MVVM/Models/ListviewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace MvvmFramework.Models
+{
+    public class ListviewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ImageIcon { get; set; }
+        public string Category { get; set; }
+        public int CurrentRating { get; set; }
+        public List<string> StarRatings { get; set; }
+        public string Postcode { get; set; }
+        public double Distance { get; set; }
+        public bool HasR { get; set; }
+        public bool HasW { get; set; }
+        public bool HasH { get; set; }
+    }
+}
