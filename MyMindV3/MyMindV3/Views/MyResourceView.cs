2467800f80a5a15d23ca8cf286ce96b49cbc2078
nodoid <paul@all-the-johnsons.co.uk>
2017-01-31 16:37:37 +0000

    Fixes

--- MyMindV3/MyMindV3/Views/MyResourceView.cs
+++ MyMindV3/MyMindV3/Views/MyResourceView.cs
@@ -21,12 +21,16 @@ namespace MyMindV3.Views
         {
             base.OnAppearing();
 
-            App.Self.PropertyChanged += (sender, e) =>
+            ViewModel.PropertyChanged += (sender, e) =>
             {
                 if (e.PropertyName == "IsBusy")
                 {
                     Device.BeginInvokeOnMainThread(() => DependencyService.Get<INetworkSpinner>().NetworkSpinner(ViewModel.IsBusy, ViewModel.SpinnerTitle, ViewModel.SpinnerMessage));
                 }
+            };
+
+            App.Self.PropertyChanged += (sender, e) =>
+            {
                 if (e.PropertyName == "NewIconRating")
                 {
                     var bc = dataList[App.Self.IdInUse];
@@ -43,9 +47,6 @@ namespace MyMindV3.Views
 
         public MyResourcesView()
         {
-            NavigationPage.SetHasNavigationBar(this, false);
-            if (Device.OS == TargetPlatform.iOS)
-                Padding = new Thickness(0, 20, 0, 0);
             BackgroundColor = Color.FromHex("022330");
             CreateUI();
         }
@@ -57,8 +58,9 @@ namespace MyMindV3.Views
             var imgGPS = new Image
             {
                 Source = "gps",
-                WidthRequest = 36,
-                HeightRequest = 36
+                HeightRequest = 28,
+                WidthRequest = 28,
+                HorizontalOptions = LayoutOptions.Center
             };
 
             postcodeSearch = new SearchBar
@@ -70,6 +72,9 @@ namespace MyMindV3.Views
                 PlaceholderColor = Color.Gray,
                 SearchCommand = new Command(() =>
                 {
+                    ViewModel.SpinnerMessage = Langs.Gen_PleaseWait;
+                    ViewModel.SpinnerTitle = Langs.Data_DownloadTitle;
+                    ViewModel.IsBusy = true;
                     ViewModel.SearchPostcode = postcodeSearch.Text.Replace(" ", "").ToLowerInvariant();
                     ViewModel.GetAvailablePostcodes();
                     ViewModel.GetUIList();
@@ -93,24 +98,23 @@ namespace MyMindV3.Views
                 BackgroundColor = Color.FromHex("022330"),
                 HorizontalOptions = LayoutOptions.Center,
                 WidthRequest = App.ScreenSize.Width,
-                HeightRequest = 42,
+                //HeightRequest = 42,
                 Children =
                 {
-                    new BoxView{BackgroundColor = Color.FromHex("022330"), WidthRequest = App.ScreenSize.Width * .1},
+                    new BoxView{BackgroundColor = Color.FromHex("022330"), WidthRequest = App.ScreenSize.Width * .12},
                     postcodeSearch,
-                    new StackLayout {WidthRequest = App.ScreenSize.Width *.15, HorizontalOptions = LayoutOptions.Start, Children = { imgGPS}}
+                    new StackLayout {WidthRequest = App.ScreenSize.Width *.13, HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, TranslationX = -8,Children = { imgGPS}}
                 }
             };
 
             menu = new MenuView(ViewModel.GetResourceFilenames(dataList?.Select(t => t.Category).ToList()));
             menu.SizeChanged += (sender, e) => { menu.HeightRequest = App.ScreenSize.Height - searchStack.HeightRequest; };
-            menu.BackgroundColor = Color.Purple;
 
             listView = new ListView
             {
                 ItemTemplate = new DataTemplate(typeof(ListViewCell)),
                 HasUnevenRows = true,
-                HeightRequest = App.ScreenSize.Height - 32,
+                HeightRequest = App.ScreenSize.Height,
                 BackgroundColor = Color.FromHex("022330"),
                 SeparatorVisibility = SeparatorVisibility.None,
                 IsPullToRefreshEnabled = true,
@@ -129,10 +133,6 @@ namespace MyMindV3.Views
                         menu.UpdateMenu(ViewModel.GetResourceFilenames(dataList?.Select(t => t.Category).ToList()));
                     });
                 });
-
-            // topbar
-
-
 
             var imgTap = new TapGestureRecognizer
             {
@@ -186,6 +186,10 @@ namespace MyMindV3.Views
             var innerStack = new StackLayout
             {
                 Orientation = StackOrientation.Horizontal,
+                TranslationY = -4,
+                HeightRequest = App.ScreenSize.Height,
+                BackgroundColor = Color.FromHex("022330"),
+                MinimumHeightRequest = App.ScreenSize.Height,
                 Children =
                 {
                     imgButton,
@@ -241,6 +245,8 @@ namespace MyMindV3.Views
                 Orientation = StackOrientation.Vertical,
                 VerticalOptions = LayoutOptions.StartAndExpand,
                 HorizontalOptions = LayoutOptions.StartAndExpand,
+                Spacing = 0,
+                Padding = new Thickness(0),
                 Children =
                 {
                     searchStack,
