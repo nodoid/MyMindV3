cbee2d0519bb2b820ac31b879eacec03d52636be
nodoid <paul@all-the-johnsons.co.uk>
2017-02-01 15:01:18 +0000

    new converter

---
+++ MyMindV3/MyMindV3/Converters/ImageTypeConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace MyMindV3.Helpers
+{
+    public class ImageTypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var prop = (string)parameter;
+            var filename = string.Empty;
+            var lc = prop.ToLowerInvariant();
+            if (lc.Contains("jpg") || lc.Contains("jpeg"))
+                filename = "jpg";
+            if (lc.Contains("png"))
+                filename = "png";
+            if (lc.Contains("pdf"))
+                filename = "pdf";
+            if (lc.Contains("doc"))
+                filename = "word";
+            return filename;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
