16f6d3e075dab8c285a4ab1c97ffc571541ce80d
nodoid <paul@all-the-johnsons.co.uk>
2017-02-02 17:28:02 +0000

    New resource model

--- MyMindV3/MVVM/Models/ResourceModel.cs
+++ MyMindV3/MVVM/Models/ResourceModel.cs
@@ -11,4 +11,17 @@
         public string ResourceContactInfo { get; set; }
         public int ResourceRating { get; set; }
     }
+
+    public class NewResourceModel
+    {
+        public int ResourceID { get; set; }
+        public string ResourceType { get; set; }
+        public string ResourceTitle { get; set; }
+        public string ResourceDescription { get; set; }
+        public string ResourceLogoLink { get; set; }
+        public string ResourceURL { get; set; }
+        public string ResourcePostcode { get; set; }
+        public bool ResourceIsDead { get; set; }
+        public bool ResourceIsNational { get; set; }
+    }
 }
