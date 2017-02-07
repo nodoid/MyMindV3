e58ac3416b48ff497ec609b3949b2018632fe8f5
nodoid <paul@all-the-johnsons.co.uk>
2017-02-07 14:20:35 +0000

    Converter for backimage

---
+++ MyMindV3/MyMindV3/Converters/CorrectBackImage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+namespace MyMindV3
+{
+    public class CorrectBackImage : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var b = (bool)value;
+            return b ? "left" : "leftgrey";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
