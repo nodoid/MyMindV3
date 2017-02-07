90636ffede74f2af9dd5706479d5dc6438739b09
nodoid <paul@all-the-johnsons.co.uk>
2017-02-07 09:28:04 +0000

    new resource ratings model

---
+++ MyMindV3/MVVM/Models/Ratings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace MvvmFramework.Models
+{
+    public class Ratings
+    {
+        public string Status { get; set; }
+        public int Rating { get; set; }
+        public int Respondents { get; set; }
+    }
+}
