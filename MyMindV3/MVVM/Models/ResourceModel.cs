61163aa0208343d781d3c89d78f3f0863b9e563b
nodoid <paul@all-the-johnsons.co.uk>
2017-02-07 14:18:00 +0000

    Modified resource models to correspond to the web api

--- MyMindV3/MVVM/Models/ResourceModel.cs
+++ MyMindV3/MVVM/Models/ResourceModel.cs
@@ -13,6 +13,7 @@ namespace MvvmFramework.Models
         public int CurrentNationalPage { get; set; }
         public int TotalNationalResouces { get; set; }
         public int TotalNationalPagesRequired { get; set; }
+        public List<ResourceModel> Resources { get; set; }
     }
 
     public class LocalResources
@@ -20,25 +21,27 @@ namespace MvvmFramework.Models
         public int CurrentLocalPage { get; set; }
         public int TotalLocalResouces { get; set; }
         public int TotalLocalPagesRequired { get; set; }
+        public List<ResourceModel> Resources { get; set; }
     }
 
     public class ResourceModel
     {
         public int ResourceID { get; set; }
-        public string ResourceType { get; set; }
+        public List<ResourceCategory> ResourceCategory { get; set; }
+        public string ResourceCategorysPiped { get; set; }
+        public int ResourceType { get; set; }
+        public string ResourceTypeTitle { get; set; }
         public string ResourceTitle { get; set; }
         public string ResourceDescription { get; set; }
         public string ResourceLogoLink { get; set; }
         public string ResourceURL { get; set; }
+        public int ResourceRating { get; set; }
+        public int ResourceNumberOfRating { get; set; }
         public string ResourcePostcode { get; set; }
         public bool ResourceIsDead { get; set; }
         public bool ResourceIsNational { get; set; }
-        public int ResourceDistance { get; set; }
-        public List<ResourceCategory> ResourceCategory { get; set; }
-        public NationalResources NationalResources { get; set; }
-        public LocalResources LocalResources { get; set; }
-        public int ResourceRating { get; set; }
-        public int ResourceNumberOfRating { get; set; }
-        public string ResourceCategorysPiped { get; set; }
+        public string ResourceDistance { get; set; }
     }
+
+
 }
