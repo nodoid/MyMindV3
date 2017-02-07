22891ecf6c5dd8ec8136d69383fa25bf75ceb1c7
nodoid <paul@all-the-johnsons.co.uk>
2017-02-07 11:27:21 +0000

    Update to include new models

--- MyMindV3/MVVM/Models/ResourceModel.cs
+++ MyMindV3/MVVM/Models/ResourceModel.cs
@@ -1,30 +1,25 @@
 ﻿using System.Collections.Generic;
 namespace MvvmFramework.Models
 {
-    /*public class ResourceModel
-    {
-        public int ResourceID { get; set; }
-        public string ResourceCategory { get; set; }
-        public string ResourceLocation { get; set; }
-        public string ResourceAddress { get; set; }
-        public string ResourceServices { get; set; }
-        public string ResourceReferrals { get; set; }
-        public string ResourceContactInfo { get; set; }
-        public int ResourceRating { get; set; }
-    }*/
-
-    public class Rating
-    {
-        public int ResourceRatingId { get; set; }
-        public int ResourceRating { get; set; }
-        public int ResourceResponded { get; set; }
-    }
-
     public class ResourceCategory
     {
         public int ResourceCategoryId { get; set; }
         public string ResourceCategoryTitle { get; set; }
         public string ResourceCategoryDescription { get; set; }
+    }
+
+    public class NationalResources
+    {
+        public int CurrentNationalPage { get; set; }
+        public int TotalNationalResouces { get; set; }
+        public int TotalNationalPagesRequired { get; set; }
+    }
+
+    public class LocalResources
+    {
+        public int CurrentLocalPage { get; set; }
+        public int TotalLocalResouces { get; set; }
+        public int TotalLocalPagesRequired { get; set; }
     }
 
     public class ResourceModel
@@ -40,6 +35,10 @@ namespace MvvmFramework.Models
         public bool ResourceIsNational { get; set; }
         public int ResourceDistance { get; set; }
         public List<ResourceCategory> ResourceCategory { get; set; }
-        public Rating ResourceRating { get; set; }
+        public NationalResources NationalResources { get; set; }
+        public LocalResources LocalResources { get; set; }
+        public int ResourceRating { get; set; }
+        public int ResourceNumberOfRating { get; set; }
+        public string ResourceCategorysPiped { get; set; }
     }
 }
