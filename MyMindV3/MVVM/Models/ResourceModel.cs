62a8c068585e4c36ab3068f3d644251bf249ea58
nodoid <paul@all-the-johnsons.co.uk>
2017-02-07 09:28:21 +0000

    Add rating and resource categories model

--- MyMindV3/MVVM/Models/ResourceModel.cs
+++ MyMindV3/MVVM/Models/ResourceModel.cs
@@ -1,6 +1,7 @@
-﻿namespace MvvmFramework.Models
+﻿using System.Collections.Generic;
+namespace MvvmFramework.Models
 {
-    public class ResourceModel
+    /*public class ResourceModel
     {
         public int ResourceID { get; set; }
         public string ResourceCategory { get; set; }
@@ -10,9 +11,23 @@
         public string ResourceReferrals { get; set; }
         public string ResourceContactInfo { get; set; }
         public int ResourceRating { get; set; }
+    }*/
+
+    public class Rating
+    {
+        public int ResourceRatingId { get; set; }
+        public int ResourceRating { get; set; }
+        public int ResourceResponded { get; set; }
     }
 
-    public class NewResourceModel
+    public class ResourceCategory
+    {
+        public int ResourceCategoryId { get; set; }
+        public string ResourceCategoryTitle { get; set; }
+        public string ResourceCategoryDescription { get; set; }
+    }
+
+    public class ResourceModel
     {
         public int ResourceID { get; set; }
         public string ResourceType { get; set; }
@@ -23,5 +38,8 @@
         public string ResourcePostcode { get; set; }
         public bool ResourceIsDead { get; set; }
         public bool ResourceIsNational { get; set; }
+        public int ResourceDistance { get; set; }
+        public List<ResourceCategory> ResourceCategory { get; set; }
+        public Rating ResourceRating { get; set; }
     }
 }
