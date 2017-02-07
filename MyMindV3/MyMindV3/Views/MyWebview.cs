a2b8ddf6f798f399b4f604b93d13672615159339
nodoid <paul@all-the-johnsons.co.uk>
2017-02-07 14:21:20 +0000

    new web view for image clicks

---
+++ MyMindV3/MyMindV3/Views/MyWebview.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace MyMindV3
+{
+    public class MyWebview : ContentPage
+    {
+        public MyWebview()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
