11bf95aa351be8f6bca2f75083e19c704c637a93
nodoid <paul@all-the-johnsons.co.uk>
2017-02-07 14:20:44 +0000

    converter for forward image

---
+++ MyMindV3/MyMindV3/Converters/CorrectForwardImage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+namespace MyMindV3
+{
+    public class CorrectForwardImage : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var b = (bool)value;
+            return b ? "right" : "rightgrey";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
