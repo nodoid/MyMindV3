8d9d689945fa883aa1ebffb89ee714a55e47e927
nodoid <paul@all-the-johnsons.co.uk>
2017-02-09 23:40:54 +0000

    ColorConverter

---
+++ MyMindV3/MyMindV3/Converters/CorrectColor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace MyMindV3
+{
+    public class CorrectColor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var b = (bool)value;
+            return b ? Color.Gray : Color.Blue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Color.Blue;
+        }
+    }
+}
+
