50d3682e6b57e77a5eca3fb35d1adacfb530975a
nodoid <paul@all-the-johnsons.co.uk>
2017-02-09 23:41:32 +0000

    Appointment List ViewCell

---
+++ MyMindV3/MyMindV3/Views/ApptList.cs
@@ -0,0 +1,68 @@
+﻿using Xamarin.Forms;
+using MyMindV3.Languages;
+
+namespace MyMindV3.Views
+{
+    public class ApptList : ViewCell
+    {
+        public ApptList()
+        {
+            var lblApptDT = new Label
+            {
+                FontSize = 15,
+                TextColor = Color.White,
+            };
+
+            var lblLoc = new Label
+            {
+                FontSize = 15,
+                TextColor = Color.White
+            };
+
+            var lblStatus = new Label
+            {
+                FontSize = 15,
+                TextColor = Color.White
+            };
+
+            lblApptDT.SetBinding(Label.TextProperty, new Binding("AppointmentDateTime"));
+            lblLoc.SetBinding(Label.TextProperty, new Binding("AppointmentLocation"));
+            lblStatus.SetBinding(Label.TextProperty, new Binding("AppointmentStatus"));
+
+            View = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                Children =
+                {
+                    new StackLayout
+                    {
+                        Orientation = StackOrientation.Horizontal,
+                        Children =
+                        {
+                            new Label {Text = Langs.MyJourney_Date, FontSize=15, TextColor = Color.White, FontAttributes=FontAttributes.Italic},
+                            lblApptDT
+                        }
+                    },
+                    new StackLayout
+                    {
+                        Orientation = StackOrientation.Horizontal,
+                        Children =
+                        {
+                            new Label {Text = Langs.MyJourney_Loc, FontSize=15, TextColor = Color.White, FontAttributes=FontAttributes.Italic},
+                            lblLoc
+                        }
+                    },
+                    new StackLayout
+                    {
+                        Orientation = StackOrientation.Horizontal,
+                        Children =
+                        {
+                            new Label {Text = Langs.MyJourney_Outcome, FontSize=15, TextColor = Color.White, FontAttributes=FontAttributes.Italic},
+                            lblStatus
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
