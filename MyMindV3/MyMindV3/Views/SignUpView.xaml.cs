2a7d2d464c60df1036f20b3ea89668727fa14b36
nodoid <paul@all-the-johnsons.co.uk>
2017-03-07 15:55:35 +0000

    Pass the messages into the VM Remove IsConnected

--- MyMindV3/MyMindV3/Views/SignUpView.xaml.cs
+++ MyMindV3/MyMindV3/Views/SignUpView.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using MvvmFramework.ViewModel;
 using Xamarin.Forms;
+using System.Collections.Generic;
+using MyMindV3.Languages;
 
 namespace MyMindV3.Views
 {
@@ -12,7 +14,11 @@ namespace MyMindV3.Views
             InitializeComponent();
 
             BindingContext = ViewModel;
-            ViewModel.IsConnected = App.Self.IsConnected;
+
+            ViewModel.Messages = new Dictionary<string, string> { { "RegUser_Completed_Message", Langs.RegUser_Completed_Message },
+                {"RegUser_Completed", Langs.RegUser_Completed}};
+            ViewModel.ErrorMessages = new Dictionary<string, string> { { "Registration_ErrorMessage", Langs.Registration_ErrorMessage } };
+            ViewModel.ErrorTitles = new Dictionary<string, string> { { "Gen_Error", Langs.Gen_Error } };
         }
     }
 }
