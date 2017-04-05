aa74597f26973288de7921c40f351d88b063d24f
nodoid <paul@all-the-johnsons.co.uk>
2017-04-05 16:11:08 +0100

    Add top bar for logout

--- MyMindV3/MVVM/ViewModel/MyLimitedMindViewModel.cs
+++ MyMindV3/MVVM/ViewModel/MyLimitedMindViewModel.cs
@@ -7,11 +7,25 @@ namespace MvvmFramework.ViewModel
     public class MyLimitedMindViewModel : BaseViewModel
     {
         INavigationService navService;
-        public MyLimitedMindViewModel(INavigationService nav,IConnectivity con)
+        public MyLimitedMindViewModel(INavigationService nav, IConnectivity con)
         {
             navService = nav;
             if (con.IsConnected)
-            SendTrackingInformation(Enums.ActionCodes.Member_Login);
+                SendTrackingInformation(Enums.ActionCodes.Member_Login);
+        }
+
+        RelayCommand logoutCommand;
+        public RelayCommand LogoutCommand
+        {
+            get
+            {
+                return logoutCommand ?? (logoutCommand = new RelayCommand(() =>
+                {
+                    DisplayLogin = true;
+                    DisplayAcceptance = false;
+                    navService.NavigateTo(ViewModelLocator.LoginKey);
+                }));
+            }
         }
 
         RelayCommand showMyProfileCommand;
